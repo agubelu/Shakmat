e3431003113d902b606a1fe29766228d21fa52d5
agubelu <agustinbd3@gmail.com>
2021-11-12 19:53:18 +0000

    Implemented Display for the movements

--- src/game_elements/movement.rs
+++ src/game_elements/movement.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter, Result};
+
 use super::{Position, PieceType};
 
 #[derive(Debug, Clone, Copy)]
@@ -22,6 +24,26 @@ impl Move {
             Move::NormalMove { from, .. } => from,
             Move::PawnPromotion { from, .. } => from,
             _ => unimplemented!()
+        }
+    }       
+}
+
+impl Display for Move {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match self {
+            Move::NormalMove { from, to } => write!(f, "{}{}", from.as_notation(), to.as_notation()),
+            Move::ShortCastle => write!(f, "O-O"),
+            Move::LongCastle => write!(f, "O-O-O"),
+            Move::PawnPromotion { from, to, promote_to } => write!(f, "{}{}={}", 
+                from.as_notation(), 
+                to.as_notation(), 
+                match promote_to {
+                    PieceType::Queen => "Q",
+                    PieceType::Rook => "R",
+                    PieceType::Bishop => "B",
+                    PieceType::Knight => "N",
+                    _ => unreachable!()
+                }),
         }
     }
 }
