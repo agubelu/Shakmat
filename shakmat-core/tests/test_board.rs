cf71ba1dfb56db24dfb5e41306f0a77ce755bfd2
agubelu <agustinbd3@gmail.com>
2022-01-14 18:35:48 +0000

    Fix incorrect position for test 6

--- shakmat-core/tests/test_board.rs
+++ shakmat-core/tests/test_board.rs
@@ -73,7 +73,7 @@ fn pos5() {
 #[test]
 fn pos6() {
     TestPosition::new(
-        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
         vec![46, 2079, 89890, 3894594, 164075551, 6923051137]
     ).run_tests()
 }
